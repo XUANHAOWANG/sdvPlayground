e07435546f84929e52f31e3a7ea67a352c037d16
XUANHAO <438164432@QQ.COM>
2021-03-09 10:22:08 +1300

    weekthree

--- src/SDV.js
+++ src/SDV.js
@@ -1,3 +1,25 @@
-console.log("hellow")
+var a="hello world"
+console.log(a)
+
+
+var a= "xuanhao"
+var b= "wang"
+console.log(a+b)
+
+
+var a =4843513 
+var b= 46843434
+var c = a +b
+console.log(c)
+
+function add(a,b){
+    return a+b
+}
+var c=add(15,43)
+console.log(c)
+
+var b=10
+a =b-1
+console.log(a)
 
 
