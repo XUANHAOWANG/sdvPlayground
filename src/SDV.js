65820c40058b3138326ed9dcce18dee011a057f8
XUANHAO <438164432@QQ.COM>
2021-03-15 12:12:34 +1300

    2

--- src/SDV.js
+++ src/SDV.js
@@ -1,24 +1,8 @@
-//JavaScript Variables
 
-// Hellow word
-var a="hello world"
+var a="hello world";
 console.log(a)
 
-//declare first name and last name
-var a= 'xuanhao'
-var b= 'wang'
-console.log(a+b)
-
-//declare a variable number
-var a =4843513 
-var b= 46843434
-var c = a +b
-console.log(c)
 
 
-let firstName ='xuanhao'
-    lastName ='wang'
-    name='Hellow every My name is'+ ' '+lastName+' '+firstName
-    console.log(name)
 
 
