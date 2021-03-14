7f1a320751747f3f63b441edd5b7ff4aff97124e
XUANHAO <438164432@QQ.COM>
2021-03-15 12:48:56 +1300

    js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 let country="New Zealand"
 let city="Nelosn"
-let postcode="7010"
+let postcode=7010
 
-console.log(country);
+console.log(typeof country);
 console.log(city);
 console.log(postcode);
+
+let listIsland=true
+let  language
+console.log(typeof listIsland)
+console.log(typeof language)
+
