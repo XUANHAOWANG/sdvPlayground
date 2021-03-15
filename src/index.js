e84af4baf0b3f789cd59668b8065f1c2a3443676
XUANHAO <438164432@QQ.COM>
2021-03-15 15:51:51 +1300

    play

--- src/index.js
+++ src/index.js
@@ -11,3 +11,48 @@ let  language
 console.log(typeof listIsland)
 console.log(typeof language)
 
+language="English";
+console.log(language)
+
+let number=10
+console.log(number)
+
+console.log(number/2)
+console.log(number>5)
+console.log(number<2)
+const address=country+','+city+','+postcode+','+language;
+console.log(address);
+
+let string='my address is .'
+console.log(string)
+
+let x='wangxuanhao'
+if (x>5){
+    console.log('草你妈妈')
+  } 
+ else{
+        console.log('false')
+    }
+
+    let A=465
+    if(A<3){
+        console.log('good')
+
+    }
+    else{
+        console.log('bad')
+    }
+
+   x=12
+    if(typeof x==='string'){
+        console.log('this is true')
+    }
+    else(
+        console.log('this is not true')
+    )
+    if(x='wangxuanhao' || typeof 'string'){
+        console.log('this is true')
+    }
+    else{
+        console.log('not true')
+    }
