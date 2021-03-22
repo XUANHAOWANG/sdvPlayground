6799b40143178f1466fd402f3e15330cd7e4dd15
XUANHAO <438164432@QQ.COM>
2021-03-22 13:58:29 +1300

    w5

---
+++ src/week5.js
@@ -0,0 +1,16 @@
+
+
+
+let person = {
+    name: 'tony',
+    age: 14,
+    driver: null
+  };
+
+
+person.driver=person.age>=16?'yes':'no';
+console.log(person.driver);
+
+let isStudent=true;
+price=isStudent=true?'$12':'$20';
+console.log(price)
